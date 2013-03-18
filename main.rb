b2d07869dd39cdd6b6cf5c3b42c3325ad9a7ef90
Gwyn Pritchett <gwyn@gwyn.ca>
2013-03-18 16:26:42 +0900

    Forms with Sinatra + Rendering Template Pages

--- main.rb
+++ main.rb
@@ -3,11 +3,33 @@ require 'sinatra'
 
 set :sessions, true
 
-get '/' do
-	erb :testing
+# Inline
+get '/inline' do
+	"Hi, directly from the action!"
 end
 
+# Template
+get '/template' do
+	erb :mytemplate
+end
 
-post '/test' do
-	@error = "This is an error"
+# Nested Template
+get '/nested_template' do
+	erb :'/users/profile'
+end
+
+# Redirect
+get '/redirect' do
+	redirect '/inline'
+end
+
+# Form
+get '/form' do
+	erb :form
+end
+
+# Post Form
+post '/form_render' do
+	data = params['some_data']
+	erb :form_render
 end
