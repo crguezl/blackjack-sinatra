f04d2e859051929a7f1d8eaa6222113b6766d6fb
Gwyn Pritchett <gwyn@gwyn.ca>
2013-03-19 15:38:32 +0900

    Storing form data into session hash

--- main.rb
+++ main.rb
@@ -3,33 +3,12 @@ require 'sinatra'
 
 set :sessions, true
 
-# Inline
-get '/inline' do
-	"Hi, directly from the action!"
-end
-
-# Template
-get '/template' do
-	erb :mytemplate
-end
-
-# Nested Template
-get '/nested_template' do
-	erb :'/users/profile'
-end
-
-# Redirect
-get '/redirect' do
-	redirect '/inline'
-end
-
-# Form
-get '/form' do
+# Home 
+get '/' do
 	erb :form
 end
 
-# Post Form
 post '/form_render' do
-	@data = params['some_data']
+	session[:some_data] = params['some_data'] # Store form data into session hash
 	erb :form_render
 end
