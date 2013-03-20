9e5fb385156ae961d34d59083a3b8dfd6ae1adc4
Gwyn Pritchett <gwyn@gwyn.ca>
2013-03-20 16:28:59 +0900

    Build initial game login, logout, styles

--- main.rb
+++ main.rb
@@ -3,12 +3,46 @@ require 'sinatra'
 
 set :sessions, true
 
-# Home 
-get '/' do
-	erb :form
+helpers do
+	
+	def user_logged_in?
+		if session[:username]
+			true
+		else
+			false
+		end
+	end
+
 end
 
-post '/form_render' do
-	session[:some_data] = params['some_data'] # Store form data into session hash
-	erb :form_render
+# Home 
+get '/' do
+	if user_logged_in?
+		redirect '/game'
+	else
+		erb :new_user
+	end	
+end
+
+get '/new_user' do
+	erb :new_user
+end
+
+post '/new_user' do
+	session[:username] = params['username']
+	if session[:username].length == 0
+		@error = "You didn't enter your name, please try again."
+		erb :new_user
+	else
+		redirect '/game'
+	end
+end
+
+get '/game' do
+	erb :game
+end
+
+get '/logout' do
+	session.clear
+	redirect '/'
 end
