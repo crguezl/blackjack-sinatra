e136d162f5361752f2bee8b8eacb375234c4f377
Gwyn Pritchett <gwyn@gwyn.ca>
2013-03-21 14:22:45 +0900

    Hiding first dealer card, deal to dealer on player stay

--- main.rb
+++ main.rb
@@ -4,44 +4,44 @@ require 'sinatra'
 set :sessions, true
 
 helpers do
-	def user_logged_in?
-		if session[:username]
-			true
-		else
-			false
-		end
-	end
+  def user_logged_in?
+    if session[:username]
+      true
+    else
+      false
+    end
+  end
 
-	def find_suit(suit)
-		ret_val = case suit
-								when 'H' then 'hearts'
-								when 'D' then 'diamonds'
-								when 'S' then 'spades'
-								when 'C' then 'clubs'
-							end
-		ret_val
-	end
+  def find_suit(suit)
+    ret_val = case suit
+      when 'H' then 'hearts'
+      when 'D' then 'diamonds'
+      when 'S' then 'spades'
+      when 'C' then 'clubs'
+    end
+    ret_val
+  end
 
-	def find_face_value(face_value)
-		ret_val = case face_value
-								when '2' then '2'
-								when '3' then '3'
-								when '4' then '4'
-								when '5' then '5'
-								when '6' then '6'
-								when '7' then '7'
-								when '8' then '8'
-								when '9' then '9'
-								when '10' then '10'
-								when 'J' then 'jack'
-								when 'Q' then 'queen'
-								when 'K' then 'king'
-								when 'A' then 'ace'
-							end
-		ret_val
-	end
+  def find_face_value(face_value)
+    ret_val = case face_value
+      when '2' then '2'
+      when '3' then '3'
+      when '4' then '4'
+      when '5' then '5'
+      when '6' then '6'
+      when '7' then '7'
+      when '8' then '8'
+      when '9' then '9'
+      when '10' then '10'
+      when 'J' then 'jack'
+      when 'Q' then 'queen'
+      when 'K' then 'king'
+      when 'A' then 'ace'
+    end
+    ret_val
+  end
 
-	def total(cards) # From Tealeaf OO solution video
+  def total(cards) # From Tealeaf OO solution video
     face_values = cards.map{|card| card[1] }
 
     total = 0
@@ -66,79 +66,87 @@ end
 
 # Home 
 get '/' do
-	if user_logged_in?
-		redirect '/game'
-	else
-		erb :new_user
-	end	
+  if user_logged_in?
+    redirect '/game'
+  else
+    erb :new_user
+  end 
 end
 
 get '/new_user' do
-	erb :new_user
+  erb :new_user
 end
 
 post '/new_user' do
-	session[:username] = params['username']
-	if session[:username].length == 0
-		@alert_message = "You didn't enter your name, please try again."
-		@alert_type = "alert-error"
-		erb :new_user
-	else
-		redirect '/game'
-	end
+  session[:username] = params['username']
+  if session[:username].length == 0
+    @alert_message = "You didn't enter your name, please try again."
+    @alert_type = "alert-error"
+    erb :new_user
+  else
+    redirect '/game'
+  end
 end
 
 get '/game' do
 
-	suits = ['H', 'D', 'C', 'S']
-	values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-	session[:deck] = suits.product(values).shuffle!
-	
-	session[:dealer_cards] = []
-	session[:player_cards] = []
+  suits = ['H', 'D', 'C', 'S']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
 
-	session[:player_cards] << session[:deck].pop
-	session[:dealer_cards] << session[:deck].pop
-	session[:player_cards] << session[:deck].pop
-	session[:dealer_cards] << session[:deck].pop
+  session[:dealer_cards] = []
+  session[:player_cards] = []
 
-	if total(session[:player_cards]) == 21
-		@alert_message = "Congratulations, you got Blackjack! You win."
-		@alert_type = "alert-success"
-		@game_over = true
-	end
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
 
-	erb :game
+  if total(session[:player_cards]) == 21
+    @alert_message = "Congratulations, you got Blackjack! You win."
+    @alert_type = "alert-success"
+    @game_over = true
+  end
+
+  erb :game
 end
 
 post '/game/hit' do
-	session[:player_cards] << session[:deck].pop
-	if total(session[:player_cards]) == 21
-		@alert_message = "Congratulations, you got Blackjack. You win!"
-		@alert_type = "alert-success"
-		@game_over = true
-	elsif total(session[:player_cards]) > 21
-		@alert_message = "Sorry, you busted. You lose."
-		@alert_type = "alert-error"
-		@game_over = true
-	end
-	erb :game
+  session[:player_cards] << session[:deck].pop
+  if total(session[:player_cards]) == 21
+    @alert_message = "Congratulations, you got Blackjack. You win!"
+    @alert_type = "alert-success"
+    @game_over = true
+  elsif total(session[:player_cards]) > 21
+    @alert_message = "Sorry, you bust. You lose."
+    @alert_type = "alert-error"
+    @game_over = true
+  end
+  erb :game
 end
 
 post '/game/stay' do
-	if total(session[:player_cards]) > total(session[:dealer_cards])
-		@alert_message = "Congratulations, your score is higher than the dealer. You win!"
-		@alert_type = "alert-success"
-		@game_over = true
-	else
-		@alert_message = "Sorry, your score isn't higher than the dealer. You lose."
-		@alert_type = "alert-error"
-		@game_over = true
-	end
-	erb :game
+
+  while total(session[:dealer_cards]) < 17
+    session[:dealer_cards] << session[:deck].pop
+  end
+  if total(session[:dealer_cards]) > 21
+    @alert_message = "Dealer bust. You win!"
+    @alert_type = "alert-success"
+    @game_over = true
+  elsif total(session[:player_cards]) > total(session[:dealer_cards])
+    @alert_message = "Congratulations, your score is higher than the dealer. You win!"
+    @alert_type = "alert-success"
+    @game_over = true
+  else
+    @alert_message = "Sorry, your score isn't higher than the dealer. You lose."
+    @alert_type = "alert-error"
+    @game_over = true
+  end
+  erb :game
 end
 
 get '/logout' do
-	session.clear
-	redirect '/'
+  session.clear
+  redirect '/'
 end
